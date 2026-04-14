8e92942576a9d79f0610acb2cdaacdaa04b7da02
Vishal Pittala <vishalpittala@PTMBA107.local>
2026-04-14 15:49:57 +0530

    fix cookie value

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -6,17 +6,16 @@ export async function POST(req: Request) {
   try {
     const body = await req.json()
 
-    const cookieStore = cookies()
-
+    const cookieStore = await cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          get(name: string) {
-            return (cookieStore as any).get(name)?.value
+            get(name: string) {
+              return cookieStore.get(name)?.value
+            },
           },
-        },
       }
     )
 
